827e9b78d09e3c71301854b42915ddecc960eafc
Victor <76717130+VictorSully79@users.noreply.github.com>
2021-07-27 23:23:14 -0700

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,10 @@
+// UberAlmostWorks
+// Victor Sullivan
+// 20210727
+// Script to show router info
+// iname = router / destdir = destination of file to write to
+//
+
 #!/bin/bash
 
 iname=$(ip -o link show | sed -rn '/^[0-9]+: en/{s/.: ([^:]*):.*/\1/p}')
